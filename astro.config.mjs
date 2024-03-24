93df129538a3a84db0583835cb56c5c34c3be7e0
Disco <jjotasaimon@gmail.com>
2024-03-24 12:14:15 +0700

    Sitemap and robots.txt added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
-  site: "https://thegodfunded.github.io",
+  integrations: [tailwind(), react(), mdx(), sitemap(), robotsTxt()],
+  site: "https://jsaimondev.github.io/"
 });
