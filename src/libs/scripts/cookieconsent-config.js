45a78c87eeab6d83a0aa1c75e94e9684e66be6e6
Disco <jjotasaimon@gmail.com>
2024-03-25 21:03:55 +0700

    GA cookies and banner ready

--- src/libs/scripts/cookieconsent-config.js
+++ src/libs/scripts/cookieconsent-config.js
@@ -26,9 +26,9 @@ CookieConsent.run({
     translations: {
       es: {
         consentModal: {
-          title: "Hola viajero, es la hora de las galletas!",
+          title: "Funded Cookies",
           description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+            "Para poder monitorizar el rendimiento de la web, utilizamos cookies de Google Analytics, y es necesario que nos des tu consentimiento. Si no te sientes cómodo con esto, también puedes rechazarlas y seguir usando la web sin problema",
           acceptAllBtn: "Aceptar todo",
           acceptNecessaryBtn: "Rechazar todo",
           showPreferencesBtn: "Gestionar preferencias",
@@ -46,25 +46,13 @@ CookieConsent.run({
             {
               title: "Uso de Cookies",
               description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            },
-            {
-              title:
-                'Cookies Estrictamente Necesarias <span class="pm__badge">Siempre Habilitado</span>',
-              description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-              linkedCategory: "necessary",
+                "Elige para qué categoría de cookies das tu permiso para alojar en tu navegador",
             },
             {
               title: "Cookies Analíticas",
               description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "Gracias a estas cookies podemos monitorizar el rendimiento de la web para seguir mejorando en el futuro.",
               linkedCategory: "analytics",
-            },
-            {
-              title: "Más información",
-              description:
-                'For any query in relation to my policy on cookies and your choices, please <a class="cc__link" href="#yourdomain.com">contact me</a>.',
             },
           ],
         },
